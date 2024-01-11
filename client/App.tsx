1552d8b65148bca8cc8dc65d2acdaeb14a5daf89
Yan Yermalayeu <yermik2014@yandex.ru>
2024-01-12 01:53:31 +0300

    feat: add gluestack config with eslint and prettier

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { config } from "@gluestack-ui/config"
+import { Box, GluestackUIProvider, Text } from "@gluestack-ui/themed"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <GluestackUIProvider config={config}>
+      <Box width="100%" height="$full" justifyContent="center" alignItems="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
+    </GluestackUIProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
